b916683bacd2737bf5b2bd5bd2b121352f0ad68d
heaven7 <532278976@qq.com>
2021-07-26 18:39:42 +0800

    opt

---
+++ pcl1/volum_detect_viewer.h
@@ -0,0 +1,12 @@
+#ifndef VOLUMDETECTVIEWER_H
+#define VOLUMDETECTVIEWER_H
+
+
+class VolumDetectViewer
+{
+public:
+    // 计算体积: https://github.com/lzir/PointCloudVolumeMeasure/blob/master/source/volDetectVisualizer.cpp
+    static void compute_volum();
+};
+
+#endif // VOLUMDETECTVIEWER_H
